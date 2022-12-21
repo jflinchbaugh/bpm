ff83e0eedb965155e6f1f276a6474f1014f1d339
John Flinchbaugh <john@hjsoft.com>
2022-12-21 12:32:23 -0500

    upgrade deps

--- project.clj
+++ project.clj
@@ -7,11 +7,11 @@
   :dependencies [[org.clojure/clojure "1.11.1"]
                  [org.clojure/clojurescript "1.11.60"]
                  [reagent "1.1.1"]
-                 [cljsjs/react "18.2.0-0"]
-                 [cljsjs/react-dom "18.2.0-0"]
+                 [cljsjs/react "18.2.0-1"]
+                 [cljsjs/react-dom "18.2.0-1"]
 ]
 
-  :plugins [[lein-cljsbuild "1.1.7"]
+  :plugins [[lein-cljsbuild "1.1.8"]
             [lein-figwheel "0.5.20"]]
 
   :clean-targets ^{:protect false}
